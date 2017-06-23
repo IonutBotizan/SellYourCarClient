684ab54d1619c0fcfc0fd35af17d5b64f44463e4
IonutBotizan <botizanionut1986@gmail.com>
2017-06-23 15:30:19 +0300

    No need to subscribe in the resolver

--- src/app/services/cars-list-resolver.service.ts
+++ src/app/services/cars-list-resolver.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {Resolve} from '@angular/router';
 import {CarsService} from "./cars.service";
 /**
- * Created by ionut on 16-6-2017.
+ * Created by ionut on 22-6-2017.
  */
 
 @Injectable()
@@ -10,6 +10,6 @@ export class CarsListResolverService implements Resolve<any>{
     constructor(private carsService: CarsService){}
 
     resolve(){
-        return this.carsService.getCars().map(cars=> cars);
+        return this.carsService.getCars();
     }
 }
