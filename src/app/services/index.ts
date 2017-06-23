cc0116e8db6e2bd46d62013cffc442e929954e52
IonutBotizan <botizanionut1986@gmail.com>
2017-06-23 15:27:23 +0300

    Update index.ts

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,8 +1,9 @@
 /**
- * Created by ionut on 16-6-2017.
+ * Created by ionut on 20-6-2017.
  */
 export * from './cars.service';
 export * from './toastr.service';
 export * from './cars-list-resolver.service';
 export * from './cars-route-activator.service';
 export  * from './iCar.model';
+export * from './config.service';
