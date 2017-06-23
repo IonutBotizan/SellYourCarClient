8f8ecf6f20ee821eb7007dee1726ba78865fc975
IonutBotizan <botizanionut1986@gmail.com>
2017-06-23 15:29:18 +0300

    Changed the mocked service into Ajax calls

--- src/app/services/cars.service.ts
+++ src/app/services/cars.service.ts
@@ -1,112 +1,50 @@
 /**
- * Created by ionut on 15-6-2017.
+ * Created by ionut on 22-6-2017.
  */
 import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs/RX';
+
 import {iCar} from "./iCar.model";
 import {Observable} from "rxjs/Observable";
+import {Http , Response} from "@angular/http";
+import {ConfigService} from "./config.service";
 @Injectable()
 export class CarsService {
+    _baseUrl: string;
+
+    constructor(private _http: Http , private _cfg : ConfigService,){
+        this._baseUrl = _cfg.getApiUrl();
+    }
+
 
     getCars()  : Observable<iCar[]>{
-        let subject = new Subject<iCar[]>();
-
-        setTimeout(()=>{
-            subject.next(CARS); subject.complete();
-        }, 100);
-
-        return subject;
-
+        return this._http.get(this._baseUrl + 'make/all_makes/model/all_models/anunturi/page')
+            .map((response: Response)=> {
+             return  <iCar[]>response.json();
+            })
+            .catch(this.handleError);
     }
-    getSingleCar(id: number): iCar {
-        return CARS.find(car=> car.id===id);
+    getSingleCar(id: number): Observable<iCar> {
+        return this._http.get(this._baseUrl +'make/all_makes/model/all_models/anunt/'+ id)
+            .map((response: Response)=>{
+               return <iCar>response.json();
+            })
+            .catch(this.handleError);
     }
+    handleError(error: any){
+        let appError = error.headers.get('Application-Error');
+        let serverError = error.json();
+        let modelStateErrors: string = '';
+        if(!serverError.type){
+            console.log(serverError);
+            for(let idx in serverError){
+                if(serverError[idx]){
+                    modelStateErrors += serverError[idx] +'\n';
+                }
+            }
+            modelStateErrors = modelStateErrors = '' ? null : modelStateErrors;
 
-    saveCar(car : iCar){
-        car.id = 543;
-        CARS.push(car);
-    }
-    updateCar(car: iCar)
-    {
-        // here we can use a predicate
-        let index = CARS.findIndex(x=> x.id == car.id);
-        CARS[index]=car;
+            return Observable.throw(appError || modelStateErrors || 'Some other server error');
+        }
     }
 }
 
-const CARS :iCar[]  =[{
-        id:1 ,
-        fullName: "Audi A4",
-        mileage: 214500 ,
-        price: 13200,
-        description : "soft and reliable",
-        imageUrl: "AudiA4_1.jpg",
-        fuel : "diesel",
-        year:2010,
-        location : "Cluj Napoca",
-        dateAdded: new Date('9/12/2016')
-
-
-    },
-    {
-        id:2 ,
-        fullName: "BMW Series 7",
-        mileage: 104500 ,
-        location: "Hunedoara",
-        price: 21200,
-        description : "luxury and reliable",
-        imageUrl: "BMWSeries7_1.jpg",
-        fuel : "diesel",
-        year: 2011,
-        dateAdded: new Date('7/10/2016')
-
-    },
-    {
-        id:3 ,
-        fullName: "Audi A6",
-        mileage: 214500 ,
-        price: 9600,
-        description : "perfect reliable and condition",
-        imageUrl: "AudiA6_1.jpg",
-        fuel : "petrol",
-        location :"Deva",
-        year: 2006,
-        dateAdded: new Date('3/2/2017')
-    },
-    {
-        id:4 ,
-        fullName: "Audi A4", location:"Baia Mare",
-        mileage: 214500 ,
-        price: 13200,
-        description : "soft and reliable",
-        imageUrl: "AudiA4_2.jpg",
-        fuel : "diesel",
-        year: 2012,
-        dateAdded: new Date('9/10/2016')
-
-    },
-    {
-        id:5 ,
-        fullName: "Toyota Avensis",
-        mileage: 214000 , location : "Hunedoara",
-        price: 9200,
-        description : "soft and reliable",
-        imageUrl: "ToyotaAvensis_1.jpg",
-        fuel : "diesel",
-        year: 2009,
-        dateAdded: new Date('7/12/2016')
-
-    },
-    {
-        id:6 ,
-        fullName: "Dacia Logan",
-        mileage: 211500 ,
-        price: 3200,
-        description : "cheapq  and reliable ",
-        imageUrl: "DaciaLogan_1.jpg",
-        fuel : "diesel",
-        year: 2005,
-        location: "Cluj Napoca",
-        dateAdded: new Date('11/12/2015')
-
-    }];
